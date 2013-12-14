6a7bb9e5d1eaa3fcfc7073a65545b91584e46637
abdul <abdul@mongolab.com>
2013-12-13 16:26:14 -0800

    Proper versioned dependency references

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='mbs_client',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'httplib2==0.7.2',
-        "maker_py==0.1.2"
+        "makerpy>=0.1.3"
     ],
     dependency_links=[
-        "https://github.com/objectlabs/maker-py/archive/master.zip#egg=maker_py-0.1.2"
+        "https://github.com/objectlabs/maker-py/archive/0.1.3.zip#egg=makerpy-0.1.3"
     ]
 
 
