29380c922887e4903f41ebe377d3c66db9fc6d61
abdul <abdul@objectlabs.com>
2014-06-09 15:53:38 -0700

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mbs_client',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[
         'httplib2==0.7.2',
