b41d3724a22c52346d14bdf90cc2ea16241a6aad
abdul <abdul@objectlabs.com>
2014-09-18 00:07:55 -0700

    updating makerpy dependencies

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     packages=find_packages(),
     install_requires=[
         'httplib2>=0.9',
-        "makerpy>=0.1.5",
+        "makerpy>=0.1.7",
         "robustify>=0.1.0"
     ],
     dependency_links=[
-        "https://github.com/objectlabs/maker-py/archive/0.1.5.zip#egg=makerpy-0.1.5",
+        "https://github.com/objectlabs/maker-py/archive/0.1.7.zip#egg=makerpy-0.1.7",
         "https://github.com/objectlabs/robustify/archive/0.1.0.zip#egg=robustify-0.1.0"
     ]
 
