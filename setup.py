e32f6c3f3cd5bb8ba503c7db466d64a72f355583
abdul <abdul@objectlabs.com>
2015-04-10 12:31:26 -0700

    update version/maker

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='mbs_client',
-    version='0.2.5',
+    version='0.2.6',
     packages=find_packages(),
     install_requires=[
         'httplib2>=0.9',
-        "makerpy>=0.1.8",
+        "makerpy>=0.1.9",
         "robustify>=0.1.0"
     ],
     dependency_links=[
-        "https://github.com/objectlabs/maker-py/archive/0.1.8.zip#egg=makerpy-0.1.8",
+        "https://github.com/objectlabs/maker-py/archive/0.1.9.zip#egg=makerpy-0.1.9",
         "https://github.com/objectlabs/robustify/archive/0.1.0.zip#egg=robustify-0.1.0"
     ]
 
