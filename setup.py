a19942ad5af46005e5e89d1866c850bf20816d8b
abdul <abdul@objectlabs.com>
2016-04-06 14:30:34 -0700

    update makerpy dependency

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='mbs_client',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     install_requires=[
         'httplib2>=0.9',
-        "makerpy>=0.3.1",
+        "makerpy>=0.3.2",
         "robustify>=0.1.0"
     ],
     dependency_links=[
-        "https://github.com/objectlabs/maker-py/archive/0.3.1.zip#egg=makerpy-0.3.1",
+        "https://github.com/objectlabs/maker-py/archive/0.3.2.zip#egg=makerpy-0.3.2",
         "https://github.com/objectlabs/robustify/archive/0.1.0.zip#egg=robustify-0.1.0"
     ]
 
