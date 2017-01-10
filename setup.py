ee9ec2e0f92c84f3101bb853e72cf24ed793a999
abdul <abdul@objectlabs.com>
2017-01-10 11:43:45 -0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mbs_client',
-    version='0.3.4',
+    version='0.3.5',
     packages=find_packages(),
     install_requires=[
         "makerpy>=0.3.4",
@@ -12,7 +12,7 @@ setup(
     dependency_links=[
         "https://github.com/objectlabs/maker-py/archive/0.3.4.zip#egg=makerpy-0.3.4",
         "https://github.com/objectlabs/robustify/archive/0.1.0.zip#egg=robustify-0.1.0",
-        "git+ssh://git@github.com/carbon-io/carbon-client-py.git@0.2.1#egg=carbonio_client-0.2.1"
+        "git+ssh://git@github.com/carbon-io/carbon-client-py.git@0.2.2#egg=carbonio_client-0.2.2"
 
     ]
 
